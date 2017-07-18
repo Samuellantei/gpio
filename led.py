69577094308ff8f1a659bcf22ec7155fe58b089a
Samuel Lantei <Samuelnsia@hotmail.com>
2017-07-18 11:39:46 +0000

    fixed led

--- led.py
+++ led.py
@@ -3,8 +3,23 @@ from time import sleep
 
 led = LED(17)
 
-while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
+def dot():
+	led.on()
+	sleep(1)
+	led.off()
+	sleep(1)
+
+def dash():
+	led.on()
+	sleep(1)
+	led.off()
+	sleep(0.5)
+
+dot()
+dash()
+dot()
+dot()
+dash()
+dot()
+dot()
+dash()
